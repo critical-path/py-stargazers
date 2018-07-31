1d04c7a2bc8c3aaf59632f6d7023b6b0c3ae2a3d
critical-path <37241479+critical-path@users.noreply.github.com>
2018-07-31 18:30:26 -0400

    Update stargazers_cli.py

--- stargazers/stargazers_cli.py
+++ stargazers/stargazers_cli.py
@@ -27,10 +27,10 @@ API = "https://api.github.com"
 def get_stargazers(user=None, repo=None, write=False):
     """Util that retrieves a list of stargazers for a given GitHub user and repo."""
 
-    if user and repo:
+    if (user is not None) and (repo is not None):
         stargazers = get_stargazers_for_one_repo(API, user, repo)
 
-    elif user and not repo:
+    if (user is not None) and (repo is None):
         repos = get_repos(API, user)
         stargazers = get_stargazers_for_all_repos(API, user, repos)
 
